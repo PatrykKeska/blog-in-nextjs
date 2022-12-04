8c401ffb6addee763e32a8b20f92e2c1c7be7cda
PatrykKeska <PatrykKeska4@gmail.com>
2022-12-04 17:57:59 +0100

    removed unnecessary files

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+    return (
+        <section >
+            <h2 >Something went wrong!</h2>
+            <h3> Please try one more time</h3>
+        </section> )
+}
+
+export default NotFound
---
+++ pages/products/index.tsx
@@ -0,0 +1,6 @@
+const Products = ()=> {
+    return <div>this is hello world</div>
+}
+
+
+export default Products;
