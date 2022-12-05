6f18e82276051acaa58080e72363ecfc84f1cc79
PatrykKeska <PatrykKeska4@gmail.com>
2022-12-05 11:39:28 +0100

    moved our BlogCard to separate component

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from 'next'
+import BlogCard from './components/BlogCard'
 interface Props {
   name: string
 }
@@ -6,17 +7,10 @@ interface Props {
 const Blogs: NextPage<Props> = () => {
   return (
     <section className="bg-green-100 max-w-3xl mx-auto p-5">
-      <div>
-        <h1 className="text-gray-900 text-3xl font-semibold">
-          Name of the blog post
-        </h1>
-        <p className="text-gray-500 text->xl">
-          Lorem ipsum dolor sit amet, consectetur adipisicing
-          elit.Blanditiisdolorem ducimus est fugiat, inventore iste magni
-          quisquam undevoluptate. A at culpa deleniti dolor iusto, maxime
-          mollitianostrum tempora voluptates?
-        </p>
-      </div>
+      <BlogCard
+        title="here is our title"
+        description="loremloremloremloremloremlorem"
+      />
     </section>
   )
 }
