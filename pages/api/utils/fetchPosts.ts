bd7d673b0c1506c8ef44ac3324b97e08fb229522
PatrykKeska <PatrykKeska4@gmail.com>
2022-12-08 16:05:31 +0100

    hotfix api path

--- pages/api/utils/fetchPosts.ts
+++ pages/api/utils/fetchPosts.ts
@@ -2,7 +2,7 @@ import { ResponseInterface } from "../../../types/Blogs/blogs.interface"
 import { apiUrl } from "./api_url"
 
 export const fetchPosts =async ()=>{
-    const res = await fetch(`${apiUrl}/posts`)
+    const res = await fetch(`${apiUrl}posts`)
     const data: ResponseInterface = await res.json()
     const { posts } = data
 
